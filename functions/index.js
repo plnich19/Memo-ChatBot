024daff6b25a27a61976d8cacd7fa1c45dcec883
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-20 13:42:35 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -11,24 +11,28 @@ let db = admin.firestore();
 var userOneDocumentRef = db.collection('users');
 
 exports.addUser = functions.region('asia-east2').https.onRequest(async (req, res) => {
-var UserId = "New Sample UserId";
-userOneDocumentRef.doc(UserId).set({
-    name: "New Ploy U",
-    pictureUrl: "1",
-})
-.then(function() {
-    console.log("Document successfully written!");
-    return "OK";
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
+
+//<---Write data part-->
+// var UserId = "New Sample UserId";
+// userOneDocumentRef.doc(UserId).set({
+//     name: "New Ploy U",
+//     pictureUrl: "1",
+// })
+// .then(function() {
+//     console.log("Document successfully written!");
+//     return "OK";
+// })
+// .catch(function(error) {
+//     console.error("Error writing document: ", error);
+// });
+//<--End write data part-->
 
 let getUsers = await getUsersData(userOneDocumentRef);
 console.log("getUsers = ",getUsers);
 getUsers.forEach(user =>{
     console.log("Users' name = ", user.name);
 })
+
 });
 
 const getUsersData = function(db){
@@ -44,4 +48,3 @@ const getUsersData = function(db){
         return UsersArray;
     })
 };
-
