b3f60076d9273d93a8b3eec01071828b964e56ca
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-20 11:28:27 +0700

    add sample write data code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const firebase = require("firebase");
 const admin = require('firebase-admin');
 const serviceAccount = require("./serviceAccountKey.json");
 const functions = require('firebase-functions');
@@ -9,3 +8,16 @@ admin.initializeApp({
   });
 
 let db = admin.firestore();
+
+var userOneDocumentRef = db.collection('users');
+userOneDocumentRef.doc("Sample UserId").set({
+    name: "Ploy U",
+    pictureUrl: "",
+})
+.then(function() {
+    console.log("Document successfully written!");
+})
+.catch(function(error) {
+    console.error("Error writing document: ", error);
+});
+
