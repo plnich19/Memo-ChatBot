a135002c79e06cb77a0c8a208f5c129368623306
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-20 13:20:46 +0700

    show user's name from the database

--- functions/index.js
+++ functions/index.js
@@ -24,4 +24,24 @@ userOneDocumentRef.doc(UserId).set({
     console.error("Error writing document: ", error);
 });
 
+let getUsers = await getUsersData(userOneDocumentRef);
+console.log("getUsers = ",getUsers);
+getUsers.forEach(user =>{
+    console.log("Users' name = ", user.name);
+})
 });
+
+const getUsersData = function(db){
+    return db.get()
+    .then (snapshot => {
+        let UsersArray = [];
+        snapshot.forEach(doc => {
+        const data = doc.data();
+          console.log(doc.id, '=>', data);
+          UsersArray.push(data);
+        });
+
+        return UsersArray;
+    })
+};
+
