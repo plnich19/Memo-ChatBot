e32f6b5ead58665cf650582b6b93fa5110b714a9
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-20 11:53:05 +0700

    export functions add users

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,20 @@ admin.initializeApp({
   });
 
 let db = admin.firestore();
-
 var userOneDocumentRef = db.collection('users');
-userOneDocumentRef.doc("Sample UserId").set({
-    name: "Ploy U",
-    pictureUrl: "",
+
+exports.addUser = functions.region('asia-east2').https.onRequest(async (req, res) => {
+var UserId = "New Sample UserId";
+userOneDocumentRef.doc(UserId).set({
+    name: "New Ploy U",
+    pictureUrl: "1",
 })
 .then(function() {
     console.log("Document successfully written!");
+    return "OK";
 })
 .catch(function(error) {
     console.error("Error writing document: ", error);
 });
 
+});
