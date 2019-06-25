692e278eb910bdb094cfc5490d91911a477167c6
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-25 11:20:39 +0700

    add postback condition

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,10 @@ const functions = require('firebase-functions');
 const line = require('@line/bot-sdk');
 
 const config = {
-    channelAccessToken: 'pBQZX8sq2ilhDSMCpmNSS4bFcalkMiV2JzFbmQTlB9cBL8yNKK6N+1xnDPJ47E0Z6Xei5pz17m+fB+TgVRyilu9rl0Dk7dvtzroqrwGysALVLhwRb1gHUx34PJsA8C2xZhFkT+uLXzKngcWRTWIYblGUYhWQfeY8sLGRXgo3xvw=',
+    channelAccessToken: 'j1ZDRGvmUdh+wz1Dtqry9xgTTotKfzRAsckUy89mZl7aGgm4dSP8eNwhz7UCYt6X6Xei5pz17m+fB+TgVRyilu9rl0Dk7dvtzroqrwGysAJV+RAO6DeFvn8YZL6jT2G9A9HBWND0SvlxD/AEzUoSr1GUYhWQfeY8sLGRXgo3xvw=',
     channelSecret: 'afc9b2ce41f4c642a1c3c3d3700dd395'
 };
+
 // create LINE SDK client
 const client = new line.Client(config);
 
@@ -22,41 +23,29 @@ exports.Chatbot = functions.region('asia-east2').https.onRequest(async (req, res
 
     const reqType = req.body.events[0].type;
     const replyToken = req.body.events[0].replyToken;
-    const reqMessage = req.body.events[0].message.text;
     if(reqType === 'message'){
-        if(reqMessage === 'เพิ่มเราหน่อย'){
+        const reqMessage = req.body.events[0].message.text;
+        if(reqMessage.toLowerCase() === 'getmember'){
             const groupId = req.body.events[0].source.groupId;
-            const userId = req.body.events[0].source.userId;
-            const userProfile = await getUserProfileById(userId);
-            const welComeMsg = `OK ${userProfile.displayName}`;
-            console.log(welComeMsg);
-            replyToRoom(groupId,welComeMsg);
-            // <---Write data part-->
-            dataOneDocumentRef.doc(groupId).collection('members').doc(userId).set({
-                displayName: userProfile.displayName,
-                pictureUrl: userProfile.pictureUrl,
-                role: "Admin"
-            })
-            .then(function() {
-                console.log("Document successfully written!");
-                return "OK";
-            })
-            .catch(function(error) {
-                console.error("Error writing document: ", error);
-            });
-            // <--End write data part-->
-        }else if(reqMessage.toLowerCase() === 'getmember'){
-            const groupId = req.body.events[0].source.groupId;
-            getMembers(groupId);
+            getMembers(groupId,db);
         }else if(reqMessage.toLowerCase().includes('getmemberprofile')){
             const userSaid = req.body.events[0].message.text;
             const groupId = req.body.events[0].source.groupId;
-            getMemberProfile(groupId,userSaid);
+            const writeTask = await getMemberProfile(groupId,userSaid,true);
+            console.log("WriteTask = ", writeTask);
         }else if(reqMessage.toLowerCase().includes('create')){
             const userSaid = req.body.events[0].message.text;
             const groupId = req.body.events[0].source.groupId;
-            createTask(groupId,userSaid,dataOneDocumentRef);
-            replyToRoom(groupId,'สร้าง task ให้เรียบร้อยแล้วน้า');
+            if(reqMessage.toLowerCase().includes('@')){
+              const userSaid = req.body.events[0].message.text;
+              const groupId = req.body.events[0].source.groupId;
+              const writeTask = await getMemberProfile(groupId,userSaid,false);
+              console.log("WriteTask = ", writeTask);
+              if(writeTask === true){
+                createTask(groupId,userSaid,dataOneDocumentRef);
+                replyToRoom(groupId,'สร้าง task ให้เรียบร้อยแล้วน้า');
+              }
+            }
         }else if(reqMessage.toLowerCase() === 'updatetask'){
             //const userSaid = req.body.events[0].message.text;
             const groupId = req.body.events[0].source.groupId;
@@ -68,7 +57,11 @@ exports.Chatbot = functions.region('asia-east2').https.onRequest(async (req, res
             const groupId = req.body.events[0].source.groupId;
             const userId = req.body.events[0].source.userId;
             updateMember(groupId,userId);
-        }
+        }else if(reqMessage.toLowerCase().includes('gettaskdetail')){
+          const userSaid = req.body.events[0].message.text;
+          const groupId = req.body.events[0].source.groupId;
+          getTaskDetail(groupId,userSaid);
+      }
         // const userSaid = req.body.events[0].message.text.toLowerCase();
         // var splitText = userSaid.split(" ");
         // if(splitText[0] === "#create"){
@@ -98,7 +91,6 @@ exports.Chatbot = functions.region('asia-east2').https.onRequest(async (req, res
         - #display เพื่อให้บอทแสดง task list ของวันนี้`;
         console.log(welComeMsg);
         replyToRoom(groupId,welComeMsg);
-        //console.log(groupId);
         replyConfirmButton(groupId);
         // const memberIds = await getGroupMemberIds(groupId);
         // console.log(memberIds);
@@ -110,7 +102,32 @@ exports.Chatbot = functions.region('asia-east2').https.onRequest(async (req, res
         const welComeMsg = `ยินดีต้อนรับ ${userProfile.displayName}`;
         console.log(welComeMsg);
         replyToRoom(groupId,welComeMsg);
+    }else if(reqType === 'postback'){
+      const postbackData = req.body.events[0].postback.data;
+      if(postbackData === 'confirm'){
+        const groupId = req.body.events[0].source.groupId;
+        const userId = req.body.events[0].source.userId;
+        const userProfile = await getUserProfileById(userId);
+        const welComeMsg = `คุณ ${userProfile.displayName} เข้าร่วมการใช้งานแล้ว`;
+        console.log(welComeMsg);
+        replyToRoom(groupId,welComeMsg);
+        // <---Write data part-->
+        dataOneDocumentRef.doc(groupId).collection('members').doc(userId).set({
+            displayName: userProfile.displayName,
+            pictureUrl: userProfile.pictureUrl,
+            role: "Admin"
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+            return "OK";
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+        // <--End write data part-->
       }
+    }
+
 
 //Call delete data function
 //DeleteUserData(userOneDocumentRef);
@@ -156,7 +173,7 @@ const replyCorouselToRoom = (groupId,UsersArray) => {
               thumbnailImageUrl: member.pictureUrl,
               title: member.displayName,
               text: member.role,
-              actions: [
+              actions: [z
                 {
                   type: "message",
                   label: "Action 1",
@@ -218,34 +235,12 @@ const getUsersData = function(db){
         let UsersArray = [];
         snapshot.forEach(doc => {
         const data = doc.data();
-        console.log(doc.id, '=>', data);
-        UsersArray.push(data);
+          console.log(doc.id, '=>', data);
+          UsersArray.push(data);
         });
 
         return UsersArray;
     })
-};
-
-const getUsername = function(array){
-    let listName = [];
-    array.forEach(user =>{
-        console.log("Users' name = ", user.displayName);
-        listName.push(user.displayName);
-    })
-    return listName;
-};
-const getUserProfileById = function(userId) {
-    return client.getProfile(userId)
-            .catch((err) => {
-            console.log('getUserProfile err',err);
-            });
-};
-
-const getGroupMemberIds = function(userId) {
-    return client.getGroupMemberIds(userId)
-            .catch((err) => {
-            console.log('getGroupMemberIds err',err);
-            });
 };
 
 const DeleteUserData = function(db){
@@ -261,35 +256,38 @@ const getMembers = async function(groupId){
     //<-- End read data part -->
 }
 
-const getMemberProfile = async function(groupId,userSaid){
+const getMemberProfile = async function(groupId,userSaid,bool){
+  var writeTask = true;
+  const isEmpty = function(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+  }
     var splitText = userSaid.split("@");
     console.log("splitText = ",splitText);
     // <-- Read data from database part -->
     let FindmembersDocumentRef = db.collection('data').doc(groupId).collection('members').where('displayName','==',splitText[1].trim());
     let getUsers = await getUsersData(FindmembersDocumentRef);
     console.log("getUsers = ",getUsers);
-    const listName = await getUsername(getUsers);
-    replyCorouselToRoom(groupId,getUsers);
+    
+    if(isEmpty(getUsers)){
+        writeTask = false;
+        const replyMsg = `ขออภัยคุณ${splitText[1]} ยังไม่ได้เปิดการใช้งานบอท คุณ${splitText[1]}โปรดยืนยันตัวตนก่อนนะคะ`;
+        replyToRoom(groupId, replyMsg);
+        replyConfirmButton(groupId);
+    }
+    else{
+      if(bool){
+        replyCorouselToRoom(groupId,getUsers);
+      }
+      else{
+        writeTask = true;
+      }
+    }
     //<-- End read data part -->
-}
-
-const updateMember = async function(groupId,userId){
-    let FindmembersDocumentRef = db.collection('data').doc(groupId).collection('members').doc(userId);
-    let transaction = db.runTransaction(t => {
-        return t.get(FindmembersDocumentRef)
-          .then(doc => {
-            // Add one person to the city population.
-            // Note: this could be done without a transaction
-            //       by updating the population using FieldValue.increment()
-            t.update(FindmembersDocumentRef, {role: "Member"});
-            return "UPDATE";
-          });
-      }).then(result => {
-        console.log('Transaction success!');
-        return "OK2";
-      }).catch(err => {
-        console.log('Transaction failure:', err);
-      });
+    return writeTask;
 }
 
 const createTask = async function(groupId,userSaid){
@@ -344,4 +342,15 @@ const getTask = async function(groupId){
     console.log("getTasks = ",getTasks);
     replyTaskCorouselToRoom(groupId,getTasks);
     //<-- End read data part -->
+}
+
+const getTaskDetail = async function(groupId,userSaid){
+  var splitText = userSaid.split(" ");
+  console.log("splitText = ",splitText);
+  // <-- Read data from database part -->
+  let FindtasksDocumentRef = db.collection('data').doc(groupId).collection('tasks').where('title','==',splitText[1]);
+  let getTask = await getUsersData(FindtasksDocumentRef);
+  console.log("getTask = ",getTask);
+  replyTaskCorouselToRoom(groupId,getTask);
+  //<-- End read data part -->
 }
