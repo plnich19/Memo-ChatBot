071c28b4143f853b1ceb8dda8ebb8cd148aa1738
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-07-17 17:38:44 +0700

    delete timestampbydate

--- functions/utils/Timestamp/tdTimestampbyDate.js
+++
@@ -1,6 +0,0 @@
-module.exports = function tdTimestampbyDate(datetime) {
-  var td = new Date(datetime);
-  var today = td.setDate(td.getDate() + 1);
-  var tdTimestampbyDate = td.setUTCHours(0, 0, 0, 0);
-  return tdTimestampbyDate;
-};
--- functions/utils/Timestamp/ytdTimestampbyDate.js
+++
@@ -1,7 +0,0 @@
-module.exports = function ytdTimestampbyDate(datetime) {
-  var date = new Date(datetime);
-  console.log(date);
-  var ytdTimestampbyDate = date.setHours(0, 0, 0, 0);
-  console.log(ytdTimestampbyDate);
-  return ytdTimestampbyDate;
-};
