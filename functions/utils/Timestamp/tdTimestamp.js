ae06824e7f99b0aab502b76bf9b2441fbf38db9d
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-07-19 14:48:15 +0700

    timestamp, replyliff changed

--- functions/utils/Timestamp/tdTimestamp.js
+++ functions/utils/Timestamp/tdTimestamp.js
@@ -1,7 +1,9 @@
 module.exports = function tdTimestamp() {
+  const HOUR = 7 * 1000 * 60 * 60;
   var td = new Date();
   var today = td.setDate(td.getDate() + 1);
   var tdTimestamp = td.setUTCHours(0, 0, 0, 0);
-  console.log("tdtimestamp", tdTimestamp);
-  return tdTimestamp;
+  var tsp = tdTimestamp - HOUR;
+  console.log("tsp", tsp);
+  return tsp;
 };
--- functions/utils/Timestamp/ytdTimestamp.js
+++ functions/utils/Timestamp/ytdTimestamp.js
@@ -1,6 +1,8 @@
 module.exports = function ytdTimestamp() {
+  const HOUR = 7 * 1000 * 60 * 60;
   var ytd = new Date();
   var ytdTimestamp = ytd.setUTCHours(0, 0, 0, 0);
-  console.log("ytdtimestamp".ytdTimestamp);
-  return ytdTimestamp;
+  var tsp = ytdTimestamp - HOUR;
+  console.log("tsp", tsp);
+  return tsp;
 };
