da386b649c646a5cbdde41d2562f8bd4e2663ef6
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-07-24 14:00:31 +0700

    updates getuserprofilebyid

--- functions/modules/Chatbot/reqTypememberJoined/index.js
+++ functions/modules/Chatbot/reqTypememberJoined/index.js
@@ -12,10 +12,6 @@ module.exports = function(dependencies) {
     const userId = req.body.events[0].joined.members[0].userId;
     const userProfile = await getUserProfileById(userId);
     const welComeMsg = `ยินดีต้อนรับ ${userProfile.displayName}
-    คำแนะนำการใช้งานน้องโน๊ต
-    - คุณ ${
-      userProfile.displayName
-    } โปรดกดยืนยันการใช้งานน้องโน๊ตในมือถือด้วยครับ
     คำสั่ง
     - #create [ชื่อ task] #to @name เพื่อสร้าง task ใหม่และมอบหมายงานให้คนๆ นั้น
     - #create [ชื่อ task] ในกรณีที่ไม่มีผู้รับงานเฉพาะเจาะจง 
@@ -37,8 +33,6 @@ module.exports = function(dependencies) {
         console.error("Error writing document: ", err);
         return "Error writing document";
       });
-
     replyToRoom(groupId, welComeMsg);
-    replyConfirmButton(replyToken);
   };
 };
--- functions/utils/Members/getUserProfileById.js
+++ functions/utils/Members/getUserProfileById.js
@@ -1,7 +1,5 @@
 module.exports = function getUserProfileById(client) {
-  return function(replyToken, userId) {
-    const reply = require("../reply/reply")(client);
-
+  return function(userId) {
     return client.getProfile(userId).catch(err => {
       console.log("getUserProfile err", err);
       reply(replyToken, "อย่าลืมแอดน้องโน๊ตเป็นเพื่อนก่อนน้า");
