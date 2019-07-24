d96c9b4ea988afc174a1eab0ca08718929efe0af
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-07-24 11:49:01 +0700

    #help instruction add

--- functions/modules/Chatbot/reqTypememberJoined/index.js
+++ functions/modules/Chatbot/reqTypememberJoined/index.js
@@ -17,9 +17,10 @@ module.exports = function(dependencies) {
       userProfile.displayName
     } โปรดกดยืนยันการใช้งานน้องโน๊ตในมือถือด้วยครับ
     คำสั่ง
-    - #Create [ชื่อ task] #to @name เพื่อสร้าง task ใหม่และมอบหมายงานให้คนๆ นั้น
-    - #Create [ชื่อ task] ในกรณีที่ไม่มีผู้รับงานเฉพาะเจาะจง 
-    - #display เพื่อให้บอทแสดง task list ของวันนี้ นายท่านสามารถแก้สถานะแล้วก็ข้อมูลของ task ได้ตรงนี้นะครับ`;
+    - #create [ชื่อ task] #to @name เพื่อสร้าง task ใหม่และมอบหมายงานให้คนๆ นั้น
+    - #create [ชื่อ task] ในกรณีที่ไม่มีผู้รับงานเฉพาะเจาะจง 
+    - #display เพื่อให้บอทแสดง task list ของวันนี้ นายท่านสามารถแก้สถานะแล้วก็ข้อมูลของ task ได้ตรงนี้นะครับ
+    - #help เพื่อดูวิธีการใช้งาน`;
     db.collection("data")
       .doc(groupId)
       .collection("members")
